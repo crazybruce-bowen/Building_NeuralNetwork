7118a60b51bf7b0c27f21105adf277ee370d2021
Bowen Li <fake.qq.com>
2022-12-01 17:55:20 +0800

    tf CNN 查看每层图像

--- code/using_tensorflow/using_tensorflow_CNN.py
+++ code/using_tensorflow/using_tensorflow_CNN.py
@@ -5,6 +5,9 @@ import tensorflow.keras as keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 from os.path import dirname
+from tensorflow.keras import models
+import numpy as np
+from matplotlib import pyplot as plt
 
 # root path
 script_path = os.path.abspath(__file__)
@@ -59,13 +62,13 @@ def create_model():
         keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
         keras.layers.MaxPooling2D(2, 2),
         # keras.layers.Conv2D(128, (3, 3), activation='relu', ),
-        keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
-        keras.layers.MaxPooling2D(2, 2),
+        # keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        # keras.layers.MaxPooling2D(2, 2),
         # Flatten
         keras.layers.Flatten(),
         # Dense
         # keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dense(8, activation='relu'),
+        # keras.layers.Dense(8, activation='relu'),
         keras.layers.Dense(1, activation='sigmoid'),
         ]
     )
@@ -90,7 +93,7 @@ class MyCallback(tf.keras.callbacks.Callback):
 
 mycallback = MyCallback()
 #%% Train and validate
-def model_train(model, epoch=5):
+def model_train(model, epoch=2):
     model.fit(
         tr_data,
         epochs=epoch,
@@ -103,10 +106,40 @@ model = model_train(model)
 #%% Model Analyse
 """
 TODO LIST:
-    1. 模型每层效果展示
+    1. 模型每层效果展示 finish
     2. history分析
 """
 
+layer_outputs = [layer.output for layer in model.layers[:2]]
+layer_names = [layer.name for layer in model.layers[:2]]
+
+# 设置一个每层都输出的模型
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+
+# 挑一个图
+pic_test = tr_data.next()[0][[0]]
+
+# 输出
+activations = activation_model.predict(pic_test)
+
+# 画图
+images_per_row = 4  # 每行8张图
+for layer_name, layer_activation in zip(layer_names, activations):
+    n_feature = layer_activation.shape[-1]  # 每层的filter数
+    size = layer_activation.shape[1]  # 图像大小
+    n_cols = n_feature//images_per_row  # 计算出图片的列数
+    display_grid = np.zeros((size*n_cols, images_per_row*size))
+    for col in range(n_cols): #行扫描
+        for row in  range (images_per_row): #平铺每行
+            channel_image = layer_activation[0, :, :, col*images_per_row+row]
+            display_grid[col*size:(col+1)*size, row*size:(row+1)*size] = channel_image #写入大图中
+    scale = 1./size #每组图缩放系数
+    plt.figure(figsize=(scale*display_grid.shape[1], scale*display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+            
+        
 #%% Transform Learning
 
 from tensorflow.keras.applications.inception_v3 import InceptionV3
